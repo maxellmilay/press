f6187f48ea712ea5414830e4d8d907ea5c4cba55
maxellmilay <milaymaxell@gmail.com>
2023-08-26 11:14:03 +0800

    [UI][Maxell] added header layout component

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="flex items-center h-20 py-2 px-5">
+      <h1>Press</h1>
+    </header>
+  );
+};
+
+export default Header;
